798585dec5168299e3d6e4c4cfaad89dce23af63
liuyuanxiao <liuyuanxiao@benmu-health.com>
2018-03-19 15:10:52 +0800

    mod: input text color defult black

--- src/main/java/com/taobao/weex/ui/component/WXInput.java
+++ src/main/java/com/taobao/weex/ui/component/WXInput.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -17,6 +17,8 @@
  * under the License.
  */
 package com.taobao.weex.ui.component;
+
+import android.graphics.Color;
 
 import com.taobao.weex.WXSDKInstance;
 import com.taobao.weex.annotation.Component;
@@ -42,6 +44,7 @@ public class WXInput extends AbstractEditComponent{
   @Override
   protected void appleStyleAfterCreated(WXEditText editText) {
     super.appleStyleAfterCreated(editText);
+    editText.setTextColor(Color.BLACK);
     editText.setSingleLine();//default use single line , same to ios
   }
 
